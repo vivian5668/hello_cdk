e266e266e4685cf1875469135726c7e4eb95f0c0
Chelsea Zhu <qianwei5668@gmail.com>
2019-04-03 18:31:47 -0700

    Initial commit

---
+++ src/main/java/com/myorg/HelloStack.java
@@ -0,0 +1,38 @@
+package com.myorg;
+
+import software.amazon.awscdk.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.iam.User;
+import software.amazon.awscdk.services.iam.UserProps;
+import software.amazon.awscdk.services.sns.Topic;
+import software.amazon.awscdk.services.sns.TopicProps;
+import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.sqs.QueueProps;
+
+public class HelloStack extends Stack {
+    public HelloStack(final Construct parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public HelloStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        Queue queue = new Queue(this, "MyFirstQueue", QueueProps.builder()
+                .withVisibilityTimeoutSec(300)
+                .build());
+
+        Topic topic = new Topic(this, "MyFirstTopic", TopicProps.builder()
+                .withDisplayName("My First Topic Yeah")
+                .build());
+
+        topic.subscribeQueue(queue);
+
+        HelloConstruct hello = new HelloConstruct(this, "Buckets", HelloConstructProps.builder()
+                .withBucketCount(5)
+                .build());
+
+        User user = new User(this, "MyUser", UserProps.builder().build());
+        hello.grantRead(user);
+    }
+}
